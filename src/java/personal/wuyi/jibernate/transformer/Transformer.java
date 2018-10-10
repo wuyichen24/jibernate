2f849df381e11a482a2ebc3137efbdc9eaaf3edc
wuyichen24 <wuyichen24@gmail.com>
2018-10-10 11:18:18 -0700

    refactoring Transform class

--- src/java/personal/wuyi/jibernate/transformer/Transformer.java
+++ src/java/personal/wuyi/jibernate/transformer/Transformer.java
@@ -1,36 +1,41 @@
 package personal.wuyi.jibernate.transformer;
 
 /**
- * Transformer.java
+ * Transformer interface for converting a source object into another. 
+ * 
+ * <p>Transformations may be chained together via decorator pattern, 
+ * consequently there is an implicit assertion that transformed objects 
+ * are similar (i.e. the implement a common interface)
  *
- * <p>Defines an interface for converting a source object into another. 
- * Transformations may be chained together via decorator pattern, consequently 
- * there is an implicit assertion that transformed objects are similar (i.e. 
- * the implement a common interface)
+ * @param  <X>
+ *         The class / type of the original object (before transforming).
+ *         
+ * @param  <Y>
+ *         The class / type of the transformed object (After transforming).
  * 
  * @author  Wuyi Chen
- *
- * @param <X>
- * @param <Y>
+ * @date    10/10/2018
+ * @version 1.0
+ * @since   1.0
  */
 @FunctionalInterface
 public interface Transformer<X,Y> {
 	/**
-	 * Transforms source object to alternate, equivalent representation
+	 * Transforms source object to alternate, equivalent representation.
 	 * 
 	 * <p>Method of transformation may be a straightforward copy between 
 	 * equivalent objects, or may result in a more complex change such as 
 	 * composition or filtering.
 	 *
 	 * @param  source
-	 * 	       The source of transformation (NON-NULL)
+	 * 	       The object needs to be transformed.
 	 *
 	 * @param  context
-	 *         Optional context
+	 *         Optional contexts.
 	 *
-	 * @return
-	 * @throws ServerException
-	 * @throws ValidationException
+	 * @return  The transformed object.
+	 * 
+     * @since   1.0
 	 */
 	Y transform(X source, Object... context);
 }
