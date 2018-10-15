ed291bb06b5eacfbd7b2d63966adfc67124c4df5
wuyichen24 <wuyichen24@gmail.com>
2018-10-15 15:11:09 -0700

    Add license to each class

--- test/unit/personal/wuyi/jibernate/entity/Ethnicity.java
+++ test/unit/personal/wuyi/jibernate/entity/Ethnicity.java
@@ -1,5 +1,29 @@
+/*
+ * Copyright 2018 Wuyi Chen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package personal.wuyi.jibernate.entity;
 
+/**
+ * The enumeration for different ethnicities.
+ * 
+ * @author  Wuyi Chen
+ * @date    10/15/2018
+ * @version 1.0
+ * @since   1.0
+ */
 public enum Ethnicity {
 	WHITE,
 	ASIAN,
